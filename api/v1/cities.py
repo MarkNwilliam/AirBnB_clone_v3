b27d1f8167c76019fbe5895e561735c992a2cec6
MarkNWiiam <nkugwamarkwiliam@gmail.com>
2023-05-29 20:35:03 +0300

    cities1

--- api/v1/cities.py
+++ api/v1/cities.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """cities"""
+
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
@@ -82,3 +83,4 @@ def updates_city(city_id):
             obj.name = request.json['name']
     storage.save()
     return jsonify(city_obj[0]), 200
+    
